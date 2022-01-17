c231b972de38de2a252aca2722c8d8cc356b9aae
Leonardo Paulo Figueiredo <leonardo.figueiredo@tecnospeed.local>
2022-01-17 17:38:40 -0300

    test: mock repository

--- src/main/java/com/lpfigueiredo/tdd/service/BookingService.java
+++ src/main/java/com/lpfigueiredo/tdd/service/BookingService.java
@@ -1,10 +1,22 @@
 package com.lpfigueiredo.tdd.service;
 
+import com.lpfigueiredo.tdd.model.BookingModel;
+import com.lpfigueiredo.tdd.repository.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Period;
+import java.util.Optional;
 
 @Service
 public class BookingService {
+
+    @Autowired
+    BookingRepository bookingRepository;
+
     public int daysCalculatorWithDatabase(String name) {
-        return 10;
+        Optional<BookingModel> bookingModelOptional = bookingRepository.findByReserveName(name);
+
+        return Period.between(bookingModelOptional.get().getCheckIn(), bookingModelOptional.get().getCheckOut()).getDays();
     }
 }
--- src/test/java/com/lpfigueiredo/tdd/BookingServiceTest.java
+++ src/test/java/com/lpfigueiredo/tdd/BookingServiceTest.java
@@ -1,13 +1,20 @@
 package com.lpfigueiredo.tdd;
 
+import com.lpfigueiredo.tdd.model.BookingModel;
+import com.lpfigueiredo.tdd.repository.BookingRepository;
 import com.lpfigueiredo.tdd.service.BookingService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
 
 @RunWith(SpringRunner.class)
 public class BookingServiceTest {
@@ -24,11 +31,26 @@ public class BookingServiceTest {
     @Autowired
     BookingService bookingService;
 
+    @MockBean
+    BookingRepository bookingRepository;
+
     @Test
     public void bookingTestServiceDaysCalculator() {
         String name = "Leonardo";
         int days = bookingService.daysCalculatorWithDatabase(name);
 
         Assertions.assertEquals(days, 10);
+    }
+
+    @Before
+    public void setup() {
+        LocalDate checkIn = LocalDate.parse("2020-11-10");
+        LocalDate checkOut = LocalDate.parse("2020-11-20");
+        BookingModel bookingModel = new BookingModel("1", "Leonardo", checkIn, checkOut, 2);
+
+        Mockito.when(bookingRepository.findByReserveName(bookingModel.getReserveName()))
+                .thenReturn(java.util.Optional.of(bookingModel));
+
+
     }
 }
